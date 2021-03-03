9bbdba65b612a7b60918ba77a165d4649a2ccb8e
stonebank <35108449>
2021-03-03 11:04:46 +0100

    Finished product - You can now save, store and load movies.

--- src/Controller.java
+++ src/Controller.java
@@ -3,10 +3,15 @@ import movie.Comedy;
 import movie.DVD;
 import movie.War;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class Controller {
 
+    private final File movie_file = new File("src/resources/movie.txt");
     private final ArrayList<DVD> movie = new ArrayList<>();
 
     private final Scanner scanner;
@@ -14,10 +19,13 @@ public class Controller {
     public Controller(Scanner scanner) {
         this.scanner = scanner;
 
+        loadMovies();
+
         // Adding some default movies
-        movie.add(new Action("Bad Boys II", "Comedy action", "2 hours 27 minutes", new String[] { "Will Smith", "Michael Eugene" }));
+        /*movie.add(new Action("Bad Boys II", "Comedy action", "2 hours 27 minutes", new String[] { "Will Smith", "Michael Eugene" }));
+        movie.add(new Action("Escape Plan", "Action thriller", "1 hour 56 minutes", new String[] { "Sylvester Stallone", "Curtis Jackson (50 cent)", "Arnold Schwarzenegger" }));
         movie.add(new War("The Hurt Locker", "2 hours 11 minutes", new String[] { "William James" }, "2001", new String[] { "USA", "Iraq" }));
-        movie.add(new Comedy("The hangover part 2", "1 hour 42 minutes", new String[] { "name 1", "name 2", "name 3" }, "Comedy/Action/Drink"));
+        movie.add(new Comedy("The hangover part 2", "1 hour 42 minutes", new String[] { "name 1", "name 2", "name 3" }, "Comedy/Action/Drink"));*/
 
         System.out.println("--- MENU ---");
         printMenu(false);
@@ -53,6 +61,9 @@ public class Controller {
             case "6":
                 printMovies();
                 break;
+            case "7":
+                saveMovies();
+                break;
             case "exit":
                 System.exit(0);
                 break;
@@ -74,6 +85,7 @@ public class Controller {
         System.out.println("Press 4 to delete a film");
         System.out.println("Press 5 to search for a film by title");
         System.out.println("Press 6 to print all movies and information");
+        System.out.println("Press 7 to save current list of movies");
         System.out.println("Type 'exit' to close the menu and exit the program.");
     }
 
@@ -96,6 +108,7 @@ public class Controller {
                 movie.add(new Action(action_title, action_subgenre, action_duration, action_mainCharacters));
 
                 System.out.println("Added a new action movie to the list!");
+                saveMovies();
                 printMenu(false);
                 break;
             case "war":
@@ -117,6 +130,7 @@ public class Controller {
                 movie.add(new War(war_title, war_duration, war_mainCharacters, time_period, countries));
 
                 System.out.println("Added a new war movie to the list!");
+                saveMovies();
                 printMenu(false);
                 break;
             case "comedy":
@@ -135,6 +149,7 @@ public class Controller {
                 movie.add(new Comedy(comedy_title, comedy_duration, comedy_mainCharacters, type));
 
                 System.out.println("Added a new comedy movie to the list!");
+                saveMovies();
                 printMenu(false);
                 break;
             default:
@@ -190,6 +205,7 @@ public class Controller {
                 continue;
             System.out.println("Successfully removed the movie title: " + movie.get(i).title());
             movie.remove(i);
+            saveMovies();
             break;
         }
 
@@ -240,6 +256,81 @@ public class Controller {
             }
         }
         printMenu(false);
+    }
+
+    private void saveMovies() {
+
+        if (movie.isEmpty()) {
+            System.err.println("There are no movies to store.");
+            return;
+        }
+
+        try {
+
+            if (movie_file.delete())
+                System.out.println("Deleted file for a new save...");
+
+            if (movie_file.createNewFile())
+                System.out.println("Created a new movie.txt, path: " + movie_file.getPath());
+
+            FileWriter writer = new FileWriter(movie_file);
+
+            for (DVD dvd : movie) {
+                if (dvd instanceof Action)
+                    writer.write("Action-" + dvd.title() + "-" + ((Action) dvd).getSubgenre() + "-" + dvd.duration() + "-" + Arrays.toString(dvd.mainCharacters()) + "\n");
+                if (dvd instanceof War)
+                    writer.write("War-" + dvd.title() + "-" + dvd.duration() + "-" + Arrays.toString(dvd.mainCharacters()) + "-" + ((War) dvd).getTimePeriod() + "-" + Arrays.toString(((War) dvd).getCountriesInvolved()) + "\n");
+                if (dvd instanceof Comedy)
+                    writer.write("Comedy-" + dvd.title() + "-" + dvd.duration() + "-" + Arrays.toString(dvd.mainCharacters()) + "-" + ((Comedy) dvd).getType() + "\n");
+            }
+
+            writer.close();
+
+            System.out.println("Successfully stored " + movie.size() + " films.");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Something went wrong upon saving the movie list...");
+        }
+
+    }
+
+    private void loadMovies() {
+
+        if (!movie_file.exists()) {
+            System.err.println("The movie file could not be found. Movies couldn't be loaded.");
+            return;
+        }
+
+        try {
+
+            Scanner reader = new Scanner(movie_file);
+
+            while (reader.hasNextLine()) {
+                String[] parameter = reader.nextLine().split("-");
+                switch (parameter[0].toLowerCase()) {
+                    case "action":
+                        movie.add(new Action(parameter[1], parameter[2], parameter[3], new String[] { parameter[4] }));
+                        break;
+                    case "war":
+                        movie.add(new War(parameter[1], parameter[2], new String[] { parameter[3] }, parameter[4], new String[] { parameter[4] }));
+                        break;
+                    case "comedy":
+                        movie.add(new Comedy(parameter[1], parameter[2], new String[] { parameter[3] }, parameter[4]));
+                        break;
+                    default:
+                        System.err.println("Could not register the type of movie");
+                }
+            }
+
+            System.out.println("Successfully loaded " + movie.size() + " stored movie data.");
+            reader.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.err.println("Something went wrong upon loading movie files...");
+        }
+
     }
 
     public Scanner getScanner() {
